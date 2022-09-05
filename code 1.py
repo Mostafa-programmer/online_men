3e496163c39d0d0fac12b46d3d248a6296ec1ef0
mohamad <112816304+mohmadalieslami@users.noreply.github.com>
2022-09-05 15:11:51 -0700

    Update code 1.py

--- code 1.py
+++ code 1.py
@@ -1,11 +1,12 @@
-from inspect import BlockFinder
 from tkinter import *
 import os
-from turtle import bgcolor 
 
 root = Tk()
 root.geometry("500x700")
-bgcolor(BlockFinder)
+
+
 root.resizable(0,0)
+
 root.title("menu fast food")
+
 root.mainloop()
